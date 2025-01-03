b61f0d6443f1aa22a30d133c91ae9346c32fc176
AldionIhzaP <aldion_ihza_pratama@tekonkrat.ac.id>
2025-01-03 17:48:54 +0700

    AldionIhzaPratama:("memperbaiki mahasiswa.ts")

--- app/models/mahasiswa.ts
+++ app/models/mahasiswa.ts
@@ -2,54 +2,59 @@
 
 import { PrismaClient } from "@prisma/client";
 
-// buat variable primsa
-const prisma = new PrismaClient(); //buat variabel "prisma"
+// buat variabel "prisma"
+const prisma = new PrismaClient();
 
-// buat fungsi tampil data
+// buat fungsi untuk ambil data mahasiswa
 export async function getData() {
-    // buat variabel "mahasiswa" untuk menampilkan data mahasiswa
-    const mahasiswa = await prisma.tb_mahasiswa.findMany({
-        where: {
-            status: "Y",
-            // jurusan: "informatika",
-            // jurusan: {
-            // startsWith: "Sistem"
-            // endsWith: "Tika" 
-            // contains: "tem"
-            // }
-        },
-    })
-    // const mahasiswa = await prisma.tb_mahasiswa.findUnique({
-    //   where: {
-    //     id: 1,
-    //   },
-    // })
-    return mahasiswa;
+  // buat variabel "mahasiswa"
+  const mahasiswa = await prisma.tb_mahasiswa.findMany({
+    where: {
+      status: "Y",
+      // prodi : {
+      //   contains : "formasi"
+      // }
+    },
+  });
+
+  return mahasiswa;
 }
 
+// buat fungsi hapus data (update status Y >> T)
+// arrow function
 
-// buat fungsi row function untuk update status
-// export async function setUpdateStatus()
-export const setUpdateStatus = async (npm: string) => {// buat variabel "mahasiswa" untuk menampilkan data mahasiswa
-    await prisma.tb_mahasiswa.updateMany({
-        where: {
-            npm: npm,
-        },
-        data: {
-            status: "N",
-        }
-    })
+// function setDelete()
+export const setUpdateStatus = async (npm: string) => {
+  await prisma.tb_mahasiswa.updateMany({
+    where: {
+      npm: npm,
+    },
+    data: {
+      status: 'N',
+    },
+  })
 }
-// buat fungsi cek data mahasiswa ("NPM")
+
+// buat fungsi check data mahasiswa (npm)
 export const checkData = async (npm: string) => {
-    const check = await prisma.tb_mahasiswa.findMany({
-        select: {
-            id: true,
-        },
-        where: {
-            npm: npm,
-        },
-    })
+  // buat variabel "check"
+  const check = await prisma.tb_mahasiswa.findMany({
+    where: {
+      npm: npm,
+    },
+  });
 
-    return check;
+  return check;
+}
+
+// buat fungsi untuk simpan data
+export const saveData = async (npm: string, nama: string, prodi: string) => {
+  await prisma.tb_mahasiswa.create({
+    data: {
+      npm: npm,
+      nama: nama,
+      prodi: prodi,
+      status: 'Y',
+    },
+  })
 }
