b7b090dfe9ba05bfb59b42de0210b333c9386898
AldionIhzaP <aldion_ihza_pratama@tekonkrat.ac.id>
2025-01-03 17:54:26 +0700

    Aldionihzapratama: ("mempebaiki fitur add agar bisa digunakan")

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,60 +1,103 @@
 "use client";
 
-import React, { useState } from 'react'
+import { get } from "http";
+import React, { useState } from "react";
+import { checkData, saveData } from "../models/mahasiswa";
+import Link from "next/link";
 
-export default function Addpage() {
-    // buat hook ("use state")
-    const [getNPM, setNPM] = useState("");
-    const [getNama, setNama] = useState("");
-    const [getJurusan, setJurusan] = useState("");
+export default function AddPage() {
+  // buat hook (use state)
+  const [getNPM, setNPM] = useState("");
+  const [getNama, setNama] = useState("");
+  const [getProdi, setProdi] = useState("");
 
-    // buat fungsi untuk simpan data
-    const setSave = () => {
-        // jika seluruh data terisi
-        // if (getNPM != "" &&
-        //     getNama != "" &&
-        //     getJurusan != "") {
-        //     alert("Data tersimpan")
-        // }
-        // jika ada data yang tidak terisi
-       
-        
-        //tenary operator
-        (getNPM !="" && getNama!="" && getJurusan!="" )
-        ?alert("Data Tersimpan")
-        :[alert("Lengkapi Seluruh Data !"),
-         alert("Gagal")]
-    }
-    return (
-        <div>
-            <div className="grid grid-cols-10 gap-2 items-center">
-                <div className="">NPM</div>
-                <div className="col-span-3 "> <input type="text" placeholder="Isi NPM" className="input input-bordered w-full " onChange={(e) => { setNPM(e.target.value) }} /> </div>
-                <div className="col-start-1">Nama</div>
-                <div className="col-span-3"><input type="text" placeholder="Isi Nama" className="input input-bordered w-full " onChange={(e) => { setNama(e.target.value) }} /></div>
-                <div className="col-start-1">Jurusan</div>
-                <div className="col-span-3"> <select defaultValue={""} className="select select-success w-full " onChange={(e) => { setJurusan(e.target.value) }}>
-                    <option disabled value={""}>Pilih Jurusan</option>
-                    <option value={"Teknik Informatika"}>Teknik Informatika</option>
-                    <option value={"Teknik Elektro"}>Teknik Elektro</option>
-                    <option value={"Teknik Sipil"}>Teknik Sipil</option>
-                    <option value={"Teknik Komputer"}>Teknik Komputer</option>
-                    <option value={"Teknik Mesin"}>Teknik Mesin</option>
-                    <option value={"Sistem Informasi"}>Sistem Informasi</option>
-                    <option value={"Teknologi Informasi"}>Teknologi Informasi</option>
-                </select></div>
-                <div className="col-start-2 col-span-2">
-                    <button className="btn btn-success m-5px w-28" onClick={setSave}>Simpan</button>
-                    <button className="btn btn-active ml-5px w-28">Batal</button>
-                </div>
-                {/* <div>className=""NPM</div>className=""
-                <div>className=""
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                </div>className=""
-            </div>className=""
-        </div>className="" */}
-            </div>
-            <div className="grid grid-cols-10 gap-2"></div>
+  // buat hook (use state)
+  // untuk respon hasil fungsi "checkData"
+  const [getValue, setValue] = useState({});
+
+  // buat fungsi untuk respon fungsi "checkData"
+  const checkNPM = async (npm: string) => {
+    setValue(await checkData(npm));
+  };
+
+  // buat fungsi simpan data
+  const setSaveData = async () => {
+    // if (getNPM == "" ||
+    //     getNama == "" ||
+    //     getProdi == ""
+    // ) {
+    //   alert("Lengkapi Seluruh Data !");
+    // } else {
+    //   alert("Ok");
+    //   alert("Yes");
+    // }
+
+    // ternary operator
+    getNPM == "" || getNama == "" || getProdi == ""
+      ? alert("Lengkapi Seluruh Data !")
+      : [
+          Object.values(getValue).length == 0
+            ? [
+                await saveData(getNPM, getNama, getProdi),
+                alert("Data Berhasil Disimpan"),
+                location.reload()
+              ]
+            : alert("NPM Sudah Pernah Digunakan !"),
+        ];
+  };
+
+  return (
+    <>
+      <title>Tambah Data Mahasiswa</title>
+      <div className="grid grid-cols-10 gap-4 items-center">
+        <div className="col-start-1">NPM</div>
+        <div className="col-span-4">
+          <input
+            type="text"
+            placeholder="Isi NPM"
+            className="input input-bordered input-success w-full"
+            onChange={(e) => {
+              setNPM(e.target.value);
+              checkNPM(e.target.value);
+            }}
+          />
         </div>
-    )
+        <div className="col-start-1">Nama</div>
+        <div className="col-span-4">
+          <input
+            type="text"
+            placeholder="Isi Nama Mahasiswa"
+            className="input input-bordered input-success w-full"
+            onChange={(e) => {
+              setNama(e.target.value);
+            }}
+          />
+        </div>
+        <div className="col-start-1">Program Studi</div>
+        <div className="col-span-4">
+          <select
+            defaultValue={""}
+            className="select select-success w-full"
+            onChange={(e) => {
+              setProdi(e.target.value);
+            }}>
+            <option value={""} disabled>
+              Pilih Program Studi Mahasiswa
+            </option>
+            <option value={"Informatika"}>Informatika</option>
+            <option value={"Sistem Informasi"}>Sistem Informasi</option>
+            <option value={"Teknologi Informasi"}>Teknologi Informasi</option>
+            <option value={"Teknik Komputer"}>Teknik Komputer</option>
+          </select>
+        </div>
+
+        <div className="col-start-2 col-span-4">
+          <button className="btn btn-neutral mr-5X w-28" onClick={setSaveData}>
+            Simpan
+          </button>
+          <Link href={"/"} className="btn btn-active ml-5X w-28">Batal</Link>
+        </div>
+      </div>
+    </>
+  );
 }
