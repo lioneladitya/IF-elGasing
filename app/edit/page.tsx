ebfc8f4f9a215d8dce0f057ca660bc4b3737fa30
AldionIP <aldion30892@gmail.com>
2025-01-10 19:37:46 +0700

    Surya Aditya : Menambahkan file page.tsx

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { checkData, saveData } from "../../models/mahasiswa";
+import { checkData, saveData } from "../models/mahasiswa";
 
 export default function EditPage({
   params,
